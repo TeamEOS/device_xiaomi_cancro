965d6e2bc0b08447769470f1d3d6273b01abd48f
Glen D'souza <glndsouza2691995@gmail.com>
2015-10-01 04:21:37 +0000

    Fix @bigrushdog 's mistakes
    Change-Id: I97124c08dd45782e90c4188e882d4a94d54168f9

--- eos.mk
+++ eos.mk
@@ -16,6 +16,9 @@
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
 # Inherit some common Eos stuff.
 $(call inherit-product, vendor/eos/config/common_full_phone.mk)
 
